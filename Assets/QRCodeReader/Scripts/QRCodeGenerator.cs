1c958d055bd53a98fbcd740347a0eb002ee44649
Ebru Dogan <ebrudoan@gmail.com>
2017-07-17 09:44:52 +0300

    Added the Unity project and updated .gitignore file.
    Unity project includes Zxing dll for Unity and sample implementation of reading and generating QR code.

---
+++ Assets/QRCodeReader/Scripts/QRCodeGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRCodeGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI() {
+
+        Texture2D myQR = generateQR("test");
+        if (GUI.Button(new Rect(300, 300, 256, 256), myQR, GUIStyle.none)) { }
+    }
+
+    public Texture2D generateQR(string text)
+    {
+        var encoded = new Texture2D(256, 256);
+        var color32 = Encode(text, encoded.width, encoded.height);
+        encoded.SetPixels32(color32);
+        encoded.Apply();
+        return encoded;
+    }
+
+    private static Color32[] Encode(string textForEncoding, int width, int height)
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+}
---
+++ Assets/QRCodeReader/Scripts/QRCodeReader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing.QrCode;
+using ZXing;
+
+public class QRCodeReader : MonoBehaviour {
+
+    private WebCamTexture _webcamTexture;
+    private Rect screenRect;
+
+	// Use this for initialization
+	void Start () {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        _webcamTexture = new WebCamTexture();
+        _webcamTexture.requestedWidth = Screen.width;
+        _webcamTexture.requestedHeight = Screen.height;
+
+        if (_webcamTexture != null)
+        {
+            _webcamTexture.Play();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI() {
+        GUI.DrawTexture(screenRect, _webcamTexture, ScaleMode.ScaleToFit);
+
+        try
+        {
+            IBarcodeReader barcodeReader = new BarcodeReader();
+
+            var result = barcodeReader.Decode(_webcamTexture.GetPixels32(), _webcamTexture.width, _webcamTexture.height);
+
+            if (result != null)
+            {
+                Debug.Log("DECODED TEXT FROM QR:" + result.Text);
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("Exception: " + e.Message);
+            throw;
+        }
+    }
+
+}
